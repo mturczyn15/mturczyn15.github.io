5591f6c3d4b2d576abc64242a21bd9192dd8abb6
mturczyn15 <mturczyn15@gmail.com>
2020-03-11 00:31:19 +0100

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-
-  $(document).ready(function() {
-  const apiRoot = 'https://frozen-depths-86903.herokuapp.com/v1/task';
-  const trelloApiRoot = 'https://frozen-depths-86903.herokuapp.com/v1/trello';
+  const apiRoot = 'https://frozen-depths-86903.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://frozen-depths-86903.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
